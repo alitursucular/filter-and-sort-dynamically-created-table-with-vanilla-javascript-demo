8403363981934f6d468f594eb8461555261971a9
Ali Turşucular <alitursucular@gmail.com>
2019-08-25 13:48:28 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,129 @@
+console.log('\nDeveloped by Ali Tursucular\n\nLinkedIn: https://www.linkedin.com/in/alitursucular/\nPersonal Portfolio: https://alitursucular.github.io');
+
+import { orders } from './orders.js';
+
+const speechBubbleClose = document.querySelectorAll('.speech-bubble-close');
+const speechBubbleRight = document.querySelector('.speech-bubble-right');
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const th = document.querySelectorAll('thead th');
+const err = document.querySelector('.err');
+
+// SPEECH BUBBLES
+speechBubbleClose.forEach((i) => {
+  i.addEventListener('click', () => {
+    i.parentElement.remove();
+  });
+});
+
+speechBubbleRight.addEventListener('mouseover', () => {
+  if (!thead.classList.contains('thead-flash-once')) {
+    thead.classList.add('thead-flash-once');
+  }
+});
+
+speechBubbleRight.addEventListener('mouseout', () => {
+  if (thead.classList.contains('thead-flash-once')) {
+    thead.classList.remove('thead-flash-once');
+  }
+});
+// SPEECH BUBBLES
+
+// TABLE CREATION
+orders.forEach((order) => {
+  const values = Object.values(order);
+  let row = tbody.insertRow(-1);
+  row.className = 'order';
+
+  for (const value of values) {
+    let cell = row.insertCell();
+    switch (value) {
+      case 'pending':
+        cell.className = 'text-warning order-item';
+        break;
+      case 'processing':
+        cell.className = 'text-info order-item';
+        break;
+      case 'completed':
+        cell.className = 'text-success order-item';
+        break;
+      default:
+        cell.className = 'order-item';
+    }
+    cell.innerHTML = value;
+  }
+});
+// TABLE CREATION
+
+// FILTER
+let filterInput = document.querySelector('.filter-input');
+const order = document.querySelectorAll('.order');
+
+filterInput.addEventListener('keyup', () => {
+  let criteria = filterInput.value.toUpperCase().trim();
+  let j = 0;
+
+  order.forEach((data) => {
+    thead.style.opacity = '1'
+    err.style.display = '';
+    speechBubbleRight.style.visibility = '';
+    if (data.innerText.toUpperCase().indexOf(criteria) > -1) {
+      data.style.display = '';
+    } else {
+      data.style.display = 'none';
+      j++;
+      if (j === order.length) {
+        thead.style.opacity = '0.2'
+        err.style.display = 'flex';
+        speechBubbleRight.style.visibility = 'hidden';
+      }
+    }
+  });
+});
+// FILTER
+
+// SORT
+let sortDirection;
+
+th.forEach((col, idx) => {
+  col.addEventListener('click', () => {
+    sortDirection = !sortDirection;
+    /** Remember: 
+      * We obtained all tr elements that has 'order' class before!
+      * However, querySelectorAll returns a NodeList, not an Array.
+      * While forEach method can be used on NodeLists, filter method cannot. 
+      * This is why we preferred to make this conversion below; where we actually need an array to filter.
+      * Note: NoteList is very similar to array and easy to convert.
+    **/
+    const rowsArrFromNodeList = Array.from(order);
+    const filteredRows = rowsArrFromNodeList.filter(item => item.style.display != 'none');
+
+    filteredRows.sort((a, b) => {
+      return a.childNodes[idx].innerHTML.localeCompare(b.childNodes[idx].innerHTML, 'en', { numeric: true, sensitivity: 'base' });
+    }).forEach((row) => {
+      sortDirection ? tbody.insertBefore(row, tbody.childNodes[tbody.length]) : tbody.insertBefore(row, tbody.childNodes[0]);
+    });
+
+  });
+});
+// SORT
+
+
+
+  // for (let i = 0; i < filteredRows.length; i++) {
+  //   let val = filteredRows[i].childNodes[idx].innerHTML.toUpperCase();
+  //   console.log(val);
+  //   let b = { val, i };
+  //   abc.push(b);
+  // }
+
+  // https://stackoverflow.com/questions/4340227/sort-mixed-alpha-numeric-array
+  //const sortAlphaNum = (a, b) => a.val.localeCompare(b.val, 'en', { numeric: true, sensitivity: 'base' });
+  //console.log(typeof abc.sort(sortAlphaNum));
+  //const a = Array.from(abc.sort(sortAlphaNum));
+  //console.log(a);
+  // a.forEach((k) => {
+  //   console.log(k);
+  //   // k.insertRow(k.i);
+  // });
+
---
+++ js/orders.js
@@ -0,0 +1,110 @@
+// https://britishsurnames.co.uk/random
+// https://www.browserling.com/tools/random-date
+
+export const orders = [
+  {
+    "orderId": 1000000145601,
+    "purchasedOn": "2019-06-05",
+    "customerName": "Ralph Clark",
+    "subtotal": "£200",
+    "status": "pending"
+  },
+  {
+    "orderId": 1000000145602,
+    "purchasedOn": "2019-04-22",
+    "customerName": "Brian Black",
+    "subtotal": "£402",
+    "status": "completed"
+  },
+  {
+    "orderId": 1000000145603,
+    "purchasedOn": "2019-04-06",
+    "customerName": "Michael Collins",
+    "subtotal": "£567",
+    "status": "processing"
+  },
+  {
+    "orderId": 1000000145604,
+    "purchasedOn": "2019-05-04",
+    "customerName": "Rebecca Fergusson",
+    "subtotal": "£239",
+    "status": "pending"
+  },
+  {
+    "orderId": 1000000145607,
+    "purchasedOn": "2019-02-12",
+    "customerName": "James Fletcher",
+    "subtotal": "£125",
+    "status": "completed"
+  },
+  {
+    "orderId": 1000000145612,
+    "purchasedOn": "2019-03-25",
+    "customerName": "Annette Collins",
+    "subtotal": "£9",
+    "status": "processing"
+  },
+  {
+    "orderId": 1000000145605,
+    "purchasedOn": "2019-07-12",
+    "customerName": "Maureen Gurney",
+    "subtotal": "£79",
+    "status": "processing"
+  },
+  {
+    "orderId": 1000000145611,
+    "purchasedOn": "2019-02-04",
+    "customerName": "Dawn Hughes",
+    "subtotal": "£218",
+    "status": "completed"
+  },
+  {
+    "orderId": 1000000145606,
+    "purchasedOn": "2019-04-20",
+    "customerName": "John Charles",
+    "subtotal": "£346",
+    "status": "pending"
+  },
+  {
+    "orderId": 1000000145613,
+    "purchasedOn": "2019-04-08",
+    "customerName": "James MacKenzie",
+    "subtotal": "£380",
+    "status": "pending"
+  },
+  {
+    "orderId": 1000000145617,
+    "purchasedOn": "2019-02-22",
+    "customerName": "Margaret Moss",
+    "subtotal": "£197",
+    "status": "completed"
+  },
+  {
+    "orderId": 1000000145615,
+    "purchasedOn": "2019-07-19",
+    "customerName": "Sian Perkins",
+    "subtotal": "£226",
+    "status": "processing"
+  },
+  {
+    "orderId": 1000000145616,
+    "purchasedOn": "2019-03-06",
+    "customerName": "Douglas Pollard",
+    "subtotal": "£138",
+    "status": "pending"
+  },
+  {
+    "orderId": 1000000145614,
+    "purchasedOn": "2019-07-03",
+    "customerName": "Susan Salter",
+    "subtotal": "£64",
+    "status": "completed"
+  },
+  {
+    "orderId": 1000000145620,
+    "purchasedOn": "2019-01-21",
+    "customerName": "Georgina Thompson",
+    "subtotal": "£309",
+    "status": "completed"
+  }
+];
